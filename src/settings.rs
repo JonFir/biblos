d39ebb9f6922b2c4364c74cf5559ac2c2f06a2b1
Evgenii Elchev <yolchev@gmail.com>
2024-04-23 18:50:42 +0700

    add env list

--- src/settings.rs
+++ src/settings.rs
@@ -15,7 +15,8 @@ pub struct Server {
 
 impl Settings {
     pub fn load() -> anyhow::Result<Self> {
-        let path = "Settings.toml";
+        let path = std::env::var("SETTINGS_FILE")
+            .context("Fail to get settings file path from environment")?;
         let settings = fs::read_to_string(path).context("Fail to read settings file")?;
         let settings = toml::from_str(&settings).context("Fail parse settings toml string")?;
         Ok(settings)
